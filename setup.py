886ab6f3a2345f5bd4801b17952770ce2116ef67
uqarni <108438834+uqarni@users.noreply.github.com>
2024-01-27 07:45:45 +0000

    pip install gepeto

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gepeto',
-    version='0.0.1',
+    version='0.0.5',
     packages=find_packages(),
     install_requires=[
         # List your project dependencies here.
